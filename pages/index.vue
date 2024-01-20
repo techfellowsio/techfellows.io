0db02f148a8cd33cb386133f2635ec360442852c
Oksid Disko <2oksid3@gmail.com>
2024-01-20 23:14:06 +0100

    build(tools): adding tw conf file

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@ const route = useRoute()
 </script>
 
 <template>
-  <div>
+  <div class="text-3xl font-bold underline text-blue-900">
     <h1>Nuxt Routing set up successfully!</h1>
     <p>Current route: {{ route.path }}</p>
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
