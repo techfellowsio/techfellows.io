d034ecb683f6683b45626784e8f9907af4e64357
Oksid Disko <2oksid3@gmail.com>
2024-03-01 21:29:01 +0100

    Add close button to BaseHeroNotification and refine text styles
    Implemented a close button within the BaseHeroNotification component for enhanced user control. Additionally, refined the text styles by removing explicit font sizes from the HTML tag and modifying the lead paragraph in _fonts.scss. These changes aim to increase app usability, efficiency, and provide a smoother user interface experience.

--- components/base/BaseHeroNotification.vue
+++ components/base/BaseHeroNotification.vue
@@ -5,6 +5,26 @@
     v-if="notification"
   >
     <div class="relative bg-brand-blue text-white transition-all">
+      <a
+        @click="isVisible = false"
+        v-if="!notification.url"
+        class="absolute right-2 top-2 z-10 block h-6 w-6 rounded-full bg-brand-dark p-0.5"
+        title="close"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </a>
       <div class="container relative flex py-4">
         <div class="flex w-full items-center justify-center gap-2">
           <div>
@@ -22,9 +42,10 @@
             </p>
           </div>
           <a
+            v-if="notification.url"
             :href="notification.url"
             :title="'adasdasds'"
-            class="button button-primary button-secondary button-sm"
+            class="button button-primary button-secondary button-sm shrink-0 px-2"
             @click="isVisible = false"
             >Explore more
           </a>
