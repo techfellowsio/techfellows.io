ce4e06db78e465c6008362970c25a467b64d0b68
jsifalda <jirka@surglogs.com>
2025-05-08 13:05:22 +0200

    feat: add qr payment into footer

--- components/base/BasePageFooter.vue
+++ components/base/BasePageFooter.vue
@@ -1,8 +1,6 @@
 <template>
   <BaseSection class="section-white mb-0 pb-0">
-    <div
-      class="flex flex-wrap justify-start gap-8 md:flex-nowrap md:justify-between"
-    >
+    <div class="justify-between gap-8 md:flex md:flex-wrap md:flex-nowrap">
       <div class="w-full space-y-8 md:w-auto md:max-w-[43%]">
         <logo></logo>
         <p class="text-balance">
@@ -10,7 +8,7 @@
           community, fostering growth and driving impactful change through
           shared knowledge & experiences.
         </p>
-        <div class="flex items-center justify-start gap-8">
+        <div class="flex items-center justify-start gap-2">
           <nuxt-link
             v-for="profile in socialMediaProfiles.getAllEnabled"
             :key="profile.name"
@@ -20,23 +18,38 @@
             <nuxt-img
               :src="profile.svg"
               :alt="profile.name + ' - logo'"
+              class="h-5 w-5"
             ></nuxt-img>
           </nuxt-link>
         </div>
       </div>
-      <!--      <div class="" v-for="menu in footerMenus.menus" :key="menu.name">-->
-      <!--        <p class="font-bold text-brand-dark">{{ menu.name }}</p>-->
-      <!--        <ul class="space-y-4">-->
-      <!--          <li v-for="link in menu.links" :key="link.url">-->
-      <!--            <nuxt-link :to="link.url" :title="link.name"-->
-      <!--              >{{ link.name }}-->
-      <!--            </nuxt-link>-->
-      <!--          </li>-->
-      <!--        </ul>-->
-      <!--      </div>-->
+      <!-- <div class="" v-for="menu in footerMenus.menus" :key="menu.name">
+        <p class="font-bold text-brand-dark">{{ menu.name }}</p>
+        <ul class="space-y-4">
+          <li v-for="link in menu.links" :key="link.url">
+            <nuxt-link :to="link.url" :title="link.name"
+              >{{ link.name }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </div> -->
+      <div class="my-8 md:my-0 md:w-1/4">
+        <p class="font-bold text-brand-dark">Support Us</p>
+        <div class="flex-row items-center justify-start md:flex md:gap-4">
+          <img
+            src="/qr-payment.png"
+            alt="QR Code for Donations"
+            class="h-32 w-32"
+          />
+          <p class="text-small space-y-4 text-gray-500">
+            TechFellows NGO at Slovakia bank account. Your contribution helps us
+            tremendously in our mission to support the tech community.
+          </p>
+        </div>
+      </div>
       <div v-if="phone || email">
         <p class="font-bold text-brand-dark">Contacts</p>
-        <ul class="space-y-4">
+        <ul class="">
           <li v-if="phone">
             <a :href="`tel:${phone.value}`">{{ phone.value }}</a>
           </li>
